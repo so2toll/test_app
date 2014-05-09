e82672167a072aa4a2267d047a7279632130008b
so2toll <steven.otu@gmail.com>
2014-05-09 08:24:40 -0700

    Finish sign in without Cucumber

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,9 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+  
   it { should be_valid }
 
   describe "when name is not present" do
@@ -85,6 +88,12 @@ describe "when password doesn't match confirmation" do
   before { @user.password_confirmation = "mismatch" }
   it { should_not be_valid }
 end
+
+describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+  
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
