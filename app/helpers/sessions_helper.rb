0d44e1b83db741fbf47d725b4bacdb967f2b6be6
so2toll <steven.otu@gmail.com>
2014-05-21 07:51:41 -0700

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,6 +21,10 @@ module SessionsHelper
     @current_user ||= User.find_by(remember_token: remember_token)
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+  
 def sign_out
     current_user.update_attribute(:remember_token,
                                   User.digest(User.new_remember_token))
@@ -28,4 +32,13 @@ def sign_out
     self.current_user = nil
   end
   
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
  end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,7 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
@@ -19,7 +20,17 @@ describe User do
   it { should respond_to(:authenticate) }
   
   it { should be_valid }
+  it { should_not be_admin }
+  
+   describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
 
+    it { should be_admin }
+  end
+  
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
